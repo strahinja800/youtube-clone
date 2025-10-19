628bf01fcb92ae10f45428ce65ecb010413c40ca
strahinja <strahinjaa20@gmail.com>
2025-10-19 22:38:53 +0200

    Added categories

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-declare global {
-  var prisma: ReturnType<typeof PrismaClient.prototype.$extends> | undefined
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-const client =
-  globalThis.prisma || new PrismaClient().$extends(withAccelerate())
+const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
-  globalThis.prisma = client
+  globalForPrisma.prisma = prisma
 }
 
-export const db = client
+export const db = prisma.$extends(withAccelerate())
--- src/scripts/seed-categories.ts
+++ src/scripts/seed-categories.ts
@@ -1,1 +1,44 @@
 // TODO: Create script seed categories
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const categoryNames = [
+  'Cars and vehicles',
+  'Comedy',
+  'Education',
+  'Gaming',
+  'Entertainment',
+  'Film and animation',
+  'How-to and style',
+  'Music',
+  'News and Politics',
+  'People and blogs',
+  'Pets and animals',
+  'Science and technology',
+  'Sports',
+  'Travel and events',
+]
+
+async function main() {
+  console.log('Seeding categories...')
+
+  try {
+    const values = categoryNames.map(name => ({
+      name,
+      description: `Videos related to ${name.toLowerCase()}`,
+    }))
+
+    await prisma.category.createMany({
+      data: values,
+    })
+
+    console.log('Categories seeded successfully')
+  } catch (error) {
+    console.error('An error occured while seeding categories', error)
+    process.exit(1)
+  }
+}
+
+main()
