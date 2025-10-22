5fc645014e7a4e23704dbae046e955f82c0b05b9
strahinja <strahinjaa20@gmail.com>
2025-10-22 15:30:43 +0200

    Added carousel

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,10 @@
-import { getCategories } from '@/actions/getCategories'
-import { CategoriesSection } from '@/modules/home/ui/sections/categories-section'
-import HomeView from '@/modules/home/ui/views/home-view'
+import HomeView from "@/modules/home/ui/views/home-view";
 
 export default async function Home() {
   return (
     <div>
-      <HomeView>
-        
-      </HomeView>
+      {/* <CategoriesSection /> */}
+      <HomeView />
     </div>
-  )
+  );
 }
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,12 +1,14 @@
-import { Suspense } from 'react'
-import { CategoriesSection } from '../sections/categories-section'
+import { Suspense } from "react";
+import { CategoriesSection } from "../sections/categories-section";
 
 export default function HomeView() {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <div className='max-w-[2400px] mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6'>
-        <CategoriesSection />
+    <>
+      <div className="max-w-[2400px] mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
+        <Suspense fallback={<p>Loading...</p>}>
+          <CategoriesSection />
+        </Suspense>
       </div>
-    </Suspense>
-  )
+    </>
+  );
 }
